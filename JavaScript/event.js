e3b1b35d2e2aa643ad3dd70e1648550aea12fd05
uniADunn <adunn300@caledonian.ac.uk>
2026-05-18 15:19:30 +0100

    Implement print functionality and update download button to trigger print dialog

--- JavaScript/event.js
+++ JavaScript/event.js
@@ -17,3 +17,9 @@ divLinks.forEach(div => {
         }
     });
 });
+
+const printBtn = document.querySelector('.pdf-download-btn .print-btn');
+printBtn.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent the default link behavior
+    window.print(); // Trigger the print dialog
+});
